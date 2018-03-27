039a6a0fcb454cac014b771415ddcbde9232007a
Mahendra Yadav <mahendra@beautifulcode.in>
2018-03-27 11:56:31 +0530

    Use logging instead of print

---
+++ client.py
@@ -0,0 +1,67 @@
+import grpc
+import time
+
+from generated_code import todo_pb2
+from generated_code import todo_pb2_grpc
+
+
+channel = grpc.insecure_channel("localhost:50051")
+
+stub = todo_pb2_grpc.TaskerStub(channel)
+
+
+task = todo_pb2.Task()
+
+task.id = int(time.strftime('%Y%m%d%H%M%S'))
+
+task.description = input("Enter task description: ")
+
+task_status_map = {'complete': todo_pb2.Task.COMPLETE,
+                   'progress': todo_pb2.Task.PROGRESS,
+                   'todo': todo_pb2.Task.TODO}
+
+task.status = task_status_map.get(input("Enter task status (options: complete, progress, todo or leave blank to fill later): "), todo_pb2.Task.UNKNOWN_TASK_STATUS)
+
+# getAllTaskList
+resp = stub.GetAllTasks(todo_pb2.GetAllTasksRequest())
+print(resp)
+
+"""
+# Working repl code
+In [52]: import grpc
+    ...: import time
+    ...:
+    ...: from generated_code import todo_pb2
+    ...: from generated_code import todo_pb2_grpc
+    ...:
+    ...:
+    ...: channel = grpc.insecure_channel("localhost:50051")
+    ...:
+    ...: stub = todo_pb2_grpc.TaskerStub(channel)
+    ...:
+    ...: task = todo_pb2.Task()
+    ...: task.id = 1 #int(time.strftime('%Y%m%d%H%M%S'))
+    ...: task.description = "some text"
+    ...: task.status = todo_pb2.Task.TODO
+    ...:
+    ...: task1 = todo_pb2.Task()
+    ...: task1.id = 2 #int(time.strftime('%Y%m%d%H%M%S'))
+    ...: task1.description = "some text"
+    ...: task1.status = todo_pb2.Task.TODO
+    ...:
+    ...: stub.CreateTask(task)
+    ...: stub.CreateTask(task1)
+    ...:
+    ...: print("Listing all tasks")
+    ...: resp = stub.GetAllTasks(todo_pb2.GetAllTasksRequest())
+    ...: print(resp)
+    ...: task2 = todo_pb2.Task(id =3)
+    ...: task2.id = 1
+    ...: stub.GetTask(task)
+    ...: print("Deleting task1")
+    ...: stub.DeleteTask(task1)
+    ...: print("Listing all tasks")
+    ...: resp = stub.GetAllTasks(todo_pb2.GetAllTasksRequest())
+    ...: print(resp)
+"""
+
--- server.py
+++ server.py
@@ -1,10 +1,14 @@
 from concurrent import futures
 import time
-
 import grpc
+import logging
+import sys
 
 from generated_code import todo_pb2
 from generated_code import todo_pb2_grpc
+
+# logging config
+logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
 
 _ONE_DAY_IN_SECONDS = 60 * 60 * 24
 
@@ -13,7 +17,8 @@ DATA_STORE = {}
 
 class Tasker(todo_pb2_grpc.TaskerServicer):
     def CreateTask(self, request, context):
-        print("Received message: %s" % request)
+        logging.info("CreateTask method started")
+        logging.info("Received message: {}".format(request))
 
         task = todo_pb2.Task()
         task.id = request.id
@@ -23,40 +28,39 @@ class Tasker(todo_pb2_grpc.TaskerServicer):
         DATA_STORE[request.id] = task
         dummy_status = todo_pb2.OperationStatus(status=1)
 
+        logging.info("CreateTask method completed")
+
         return todo_pb2.CreateTaskResponse(op_status=dummy_status)
 
     def GetAllTasks(self, request, context):
-        print("Listing all tasks")
-        return todo_pb2.TasksList(tasks=DATA_STORE.values())
+        logging.info("Listing all tasks")
+        response = todo_pb2.TasksList(tasks=DATA_STORE.values())
+        logging.info(response)
+
+        return response
 
     def GetTask(self, request, context):
-        print("Received message: %s" % request)
-        task = DATA_STORE.get(request.id, None)
-        print("Got data", task)
-        #  import pdb;pdb.set_trace()
-        if task is not None:
-            print("Returning ", task)
-            return task  # todo_pb2.Task(id=task.id, description=task.description, status=task.status)
-        else:
-            return todo_pb2.Task()
+        logging.info("Received message: {}".format(request))
+        task = DATA_STORE.get(request.id, todo_pb2.Task())
+        logging.info("Got data: {}".format(task))
+        return task
 
     def DeleteTask(self, request, context):
+        logging.info("Delete task with ID: {}".format(request.id))
         task = DATA_STORE.pop(request.id, None)
 
         success_status = todo_pb2.OperationStatus(status=1)
         failed_status = todo_pb2.OperationStatus(status=2)
         if task is not None:
-
             return todo_pb2.DeleteTaskResponse(op_status=success_status)
         else:
             return todo_pb2.DeleteTaskResponse(op_status=failed_status)
-            #  return "Error: Task with id: {} is not found".format(request.id)
 
 
 def serve():
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
     todo_pb2_grpc.add_TaskerServicer_to_server(Tasker(), server)
-    print("Starting server. Listening on port 50051.")
+    logging.info("Starting server. Listening on port 50051.")
     server.add_insecure_port('[::]:50051')
     server.start()
     try:
